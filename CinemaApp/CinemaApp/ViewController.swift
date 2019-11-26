64f7ec0836e8ad4fc6b595e8b93ddeb7dc696a0a
Ana-Popilian <ana.popilian@icloud.com>
2019-11-26 11:32:01 +0100

    Initial commit

---
+++ CinemaApp/CinemaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinemaApp
+//
+//  Created by Ana on 26/11/2019.
+//  Copyright © 2019 Ana Popilian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
