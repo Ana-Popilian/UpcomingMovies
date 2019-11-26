9fc781adf7e12ae1808408d1ef5d8661b784e0b0
Ana-Popilian <ana.popilian@icloud.com>
2019-11-26 14:40:32 +0100

    Created structure for app

---
+++ CinemaApp/CinemaApp/Scenes/MovieList/MovieListView.swift
@@ -0,0 +1,10 @@
+//
+//  MovieListView.swift
+//  CinemaApp
+//
+//  Created by Ana on 26/11/2019.
+//  Copyright © 2019 Ana Popilian. All rights reserved.
+//
+
+import UIKit
+
---
+++ CinemaApp/CinemaApp/Services/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  CinemaApp
+//
+//  Created by Ana on 26/11/2019.
+//  Copyright © 2019 Ana Popilian. All rights reserved.
+//
+
+import Foundation
